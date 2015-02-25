3a9865b05de026abb289d7a3335a19d3234aaa3c
Adolfo Builes <builes.adolfo@gmail.com>
2015-02-25 00:14:09 -0500

    Add test containing ember-qunit.

--- tests/qunit-migrator-test.js
+++ tests/qunit-migrator-test.js
@@ -7,22 +7,18 @@ var recast = require('recast');
 // skip if import { module, test } from 'qunit' is defined
 // add import { module, test } from 'qunit' if ember-qunit is not used
 // or include skip if used
-// add assert to test func
-// replace assert
-// replace ok, equal, etc... with assert.ok
 // if is component test, replace this.append(); with this.render();
-// use beforeEach and afterEach instead of setup and teardown inside module
 
 
-// describe('Qunit tests with ember-qunit', function() {
-//   it('makes the correct transformations', function() {
-//     var source = fs.readFileSync('./tests/fixtures/qunit-files/old-with-ember-qunit.js');
-//     var watson = new Watson();
-//     var newSource = watson._transformQUnitTest(source);
+describe('Qunit tests with ember-qunit', function() {
+  it('makes the correct transformations', function() {
+    var source = fs.readFileSync('./tests/fixtures/qunit-files/old-with-ember-qunit.js');
+    var watson = new Watson();
+    var newSource = watson._transformQUnitTest(source);
 
-//     astEquality(newSource, fs.readFileSync('./tests/fixtures/qunit-files/new-with-ember-qunit.js'));
-//   });
-// });
+    astEquality(newSource, fs.readFileSync('./tests/fixtures/qunit-files/new-with-ember-qunit.js'));
+  });
+});
 
 describe('Qunit tests only with qunit', function() {
   it('makes the correct transformations', function() {
