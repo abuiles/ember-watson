509968951fcd2e8670bdc559730cad783663a742
Adolfo Builes <builes.adolfo@gmail.com>
2015-04-06 08:58:40 -0500

    Include `import Ember from 'ember'` if not defined (closes #24).

---
+++ tests/fixtures/prototype-extension-files/old-without-ember.js
@@ -0,0 +1,33 @@
+export default Ember.Controller.extend({
+  hasNoDependency: function() {
+    return true;
+  }.property(),
+
+  hasMultipleDependencies: function() {
+    return this.getProperties('foo', 'bar');
+  }.property('foo', 'bar'),
+
+  chainedCP: function() {
+    return false;
+  }.property('foo').volatile().readOnly(),
+
+  isNotExtendingPrototype: Ember.computed(function() {
+    return true;
+  }),
+
+  isNotExtendingPrototypeWithDependencies: Ember.computed('foo', function() {
+    return this.get('foo');
+  }),
+
+  simpleObserver: function() {
+    this.set('bar', true);
+  }.observes('foo'),
+
+  chainedObserver: function(property) {
+    this.set('baz', true);
+  }.observes('foo', 'bar').on('init'),
+
+  onInit: function() {
+    this.set('foobar', true);
+  }.on('init')
+});
