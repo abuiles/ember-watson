8912217b5842c90dbff91f5f9e267dd425884ab6
Stanley Stuart <stanley@stan.li>
2015-04-16 16:02:50 -0500

    failing test for app global case

--- tests/ember-data-model-lookups-test.js
+++ tests/ember-data-model-lookups-test.js
@@ -7,17 +7,35 @@ var recast      = require('recast');
 
 describe('ember data model lookups', function(){
   var baseDir = './tests/fixtures/ember-data-model-lookups';
+  var watson;
+
+  beforeEach(function(){
+    watson = new Watson();
+  });
 
   describe('hasMany relationship macro', function(){
 
-    it('migrates to dasherized', function(){
-      var source            = fs.readFileSync(baseDir + '/old.js');
-      var watson            = new Watson();
-      var newSource         = watson._transformEmberDataModelLookups(source);
-      var expectedNewSource = fs.readFileSync(baseDir + '/new.js');
+    describe('when using the string form', function(){
+      it('migrates to dasherized', function(){
+        var source            = fs.readFileSync(baseDir + '/old.js');
+        var newSource         = watson._transformEmberDataModelLookups(source);
+        var expectedNewSource = fs.readFileSync(baseDir + '/new.js');
 
-      astEquality(newSource, expectedNewSource);
+        astEquality(newSource, expectedNewSource);
+      });
     });
+
+    describe('when using the object form', function(){
+
+      it('migrates to a dasherized string', function(){
+        var source         = fs.readFileSync(baseDir + '/app-global-old.js');
+        var newSource      = watson._transformEmberDataModelLookups(source);
+        var expectedSource = fs.readFileSync(baseDir + '/app-global-new.js');
+
+        astEquality(newSource, expectedNewSource);
+      });
+    });
+
   });
 
 });
