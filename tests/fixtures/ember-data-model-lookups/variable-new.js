2abff73a571b1b3775703cf6d095de9b7292a2a8
Stanley Stuart <stanley@stan.li>
2015-04-16 16:20:35 -0500

    refactor extractName to its own function

--- tests/fixtures/ember-data-model-lookups/variable-new.js
+++ tests/fixtures/ember-data-model-lookups/variable-new.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 import Post from 'my-app/models/post';
 
 export default DS.Model.extend({
-  messages: DS.hasMany(Post, {async: true})
+  messages: DS.hasMany('post', {async: true})
 });
