242b54fab5c4b4abae1be85718c3868ec1fa6286
Adolfo Builes <builes.adolfo@gmail.com>
2015-04-17 09:56:27 -0500

    Do expose new command.

--- addon/commands/index.js
+++ addon/commands/index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   'watson:upgrade-qunit-tests': require('./upgrade-qunit-tests'),
-  'watson:convert-prototype-extensions': require('./convert-prototype-extensions')
+  'watson:convert-prototype-extensions': require('./convert-prototype-extensions'),
+  'watson:convert-ember-data-model-lookups': require('./convert-ember-data-model-lookups')
 };
